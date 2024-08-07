6b4ce195e3fce780a4651f9400decf1025f4cff9
michelcosta7 <michelifmtads@gmail.com>
2024-08-06 21:13:41 -0400

    initial

---
+++ src/main/java/com/mac/maccloud/MaccloudApplication.java
@@ -0,0 +1,14 @@
+package com.mac.maccloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaccloudApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MaccloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mac/maccloud/MaccloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mac.maccloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaccloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
